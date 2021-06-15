04aa9cd6a287b881f9be455b290ba5daf2c846e9
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-06-15 20:18:30 +0300

    [JS IR] Add test with boolean in external interface

---
+++ js/js.translator/testData/box/propertyAccess/booleanInExternals.kt
@@ -0,0 +1,38 @@
+fun box(): String {
+    val interfaceWithBoolean: InterfaceWithBoolean = js("{}")
+    C().c = interfaceWithBoolean.foo
+
+    return "OK"
+}
+
+abstract class A<T> {
+    open fun get(): T {
+        return this.asDynamic()["attr"].unsafeCast<T>()
+    }
+
+    open fun set(value: T) {
+        this.asDynamic()["attr"] = value
+    }
+}
+
+class B : A<Boolean>() {
+    override fun set(value: Boolean) {
+        if (value) {
+            this.asDynamic()["attr"] = value
+        }
+    }
+}
+
+val b: A<Boolean> = B()
+
+class C {
+    var c: Boolean
+        get() = b.get()
+        set(newValue) {
+            b.set(newValue)
+        }
+}
+
+external interface InterfaceWithBoolean {
+    var foo: Boolean
+}
